4acc251af4210596a5583336ad1fbbcb22892d07
“Carlos <cartobon@gmail.com>
2024-02-27 09:41:57 -0500

    Upload of Spring GraphQL example

---
+++ introGraphql/src/main/java/com/example/intrographql/IntroGraphqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.intrographql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroGraphqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntroGraphqlApplication.class, args);
+    }
+
+}
---
+++ introGraphql/src/test/java/com/example/intrographql/IntroGraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.intrographql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroGraphqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
